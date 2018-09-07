af41904103e88c3e517949270151dd50f2b12cb8
Conrad Kelly <NorthStar@NorthStars-MacBook-Pro.local>
2018-09-07 15:33:00 -0500

    add test DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,45 @@ describe('GET /todos/:id', () => {
     });
 });
 
+describe('DELETE /toos/:id', () => {
+    it('should delete a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId)
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+        });
+
+        Todo.findById(hexId).then((todo) => {
+            expect(null).toNotExist();
+            done();  
+        }).catch ((e) => done(e));
+       
+
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+        
+        request(app)
+        .delete(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done);
+
+    });
+
+    it('should return a 404 id object is invalid', (done) => {
+
+        request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done);
+
+    }); 
+});
