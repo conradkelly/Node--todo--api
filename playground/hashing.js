54b7805465edb4686ecb8a53a23c2b095ad5b130
Conrad Kelly <NorthStar@NorthStars-MacBook-Pro.local>
2018-09-11 20:28:36 -0500

    add hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,22 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = '123abc!';
+/* bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+ */
+var hashedPassword = '$2a$10$R/ESSFuttogOhaWVhNZEbuyk17CLYWiCHNlfSJgWaZbvlLiyxjtAi'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res)
+})
+
+
+/* var data = {
     id: 10
 };
 
@@ -9,7 +24,7 @@ var token = jwt.sign(data, 'vikings');
 console.log(token);
 
 var decoded = jwt.verify(token, 'vikings');
-console.log(decoded);
+console.log(decoded); */
 
 
 /* var message = 'I am user number 3';
